f88373e83f796c449398888fb14b0bd623baa517
Scott-Gauthier <gauthier.scott@gmail.com>
2022-08-28 15:59:00 -0700

    Added in comments.

--- js/script.js
+++ js/script.js
@@ -10,65 +10,63 @@ function writePassword(event) {
 }
 
 function generatePassword() {
-
+//create variable with desired password size
   var passwordSize = prompt("Please enter length of password between 8 and 128 characters.");
-
   if ( 8 <= passwordSize && passwordSize <= 128 ) {
-    var lowercase = prompt("Do you want to include lowercase? y/n");
-    var uppercase = prompt("Do you want to include uppercase? y/n");
-    var numeric = prompt("Do you want to include numbers? y/n");
-    var specialCharacters= prompt("Do you want to include special characters(`!#$%&()*+,-./:;<=>?@[]^_~`)? y/n");
-    var possible = "";
-    var alertNoty = 0;
-  
-    if (lowercase == 'y') {
-        possible += 'abcdefghijklmnopqrstuvwxyz';
-        console.log(possible);
+    //variables to store additional prompt answers if answer is in acceptable range.
+      var lowercase = prompt("Do you want to include lowercase? y/n");
+      var uppercase = prompt("Do you want to include uppercase? y/n");
+      var numeric = prompt("Do you want to include numbers? y/n");
+      var specialCharacters= prompt("Do you want to include special characters(`!#$%&()*+,-./:;<=>?@[]^_~`)? y/n");
+      var possible = "";
+      var alertNoty = 0;
+      var text = "";
+
+      //process variable prompt answer for lowercase to set possible character sting set used to generage the password
+      if (lowercase == 'y') {
+          possible += 'abcdefghijklmnopqrstuvwxyz';
+      } else {
+        alertNoty--;
+      }
+      //process variable prompt answer for uppercase to set possible character sting set used to generage the password
+      if (uppercase == 'y') {
+          possible += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      } else {
+        alertNoty--;
+      }
+      //process variable prompt answer for numeric to set possible character sting set used to generage the password
+      if (numeric == 'y') {
+          possible += '0123456789';
+      } else {
+        alertNoty--;
+      }
+      //process variable prompt answer for specialCharacters to set possible character sting set used to generage the password
+      if (specialCharacters == 'y') {
+          possible += `!#$%&()*+,-./:;<=>?@[]^_~`;
+      } else {
+        alertNoty--;
+      }
     } else {
-      alertNoty--;
-      console.log(alertNoty);
-    }
-    if (uppercase == 'y') {
-        possible += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        console.log(possible);
-    } else {
-      alertNoty--;
-      console.log(alertNoty);
-    }
-    if (numeric == 'y') {
-        possible += '0123456789';
-        console.log(possible);
-    } else {
-      alertNoty--;
-      console.log(alertNoty);
-    }
-    if (specialCharacters == 'y') {
-        possible += `!#$%&()*+,-./:;<=>?@[]^_~`;
-        console.log(possible);
-    } else {
-      alertNoty--;
-      console.log(alertNoty);
-    }
-    } else {
+    //This sets the alert variable and alerts if the characters is not in the right range.
       alertNoty = -6;
-      console.log(alertNoty);
       alert("Password needs to be between 8 and 128 characters.");
-  }
-  console.log(alertNoty);
-  var text = "";
+    }
+  //This is what runs if answer is within character range and at least one additional prompt answered y
   if (alertNoty >= -3 ) {
       for (var i = 0; i < passwordSize; i++) {
             text += possible.charAt(Math.floor(Math.random() * possible.length));
       }
       return text;
+    //This is what sets the form field if characters wanted is not within acceptable range.
   } else if (alertNoty == -6) {
     return text = `Password needs to be between 8 and 128 characters.`;
+    //this is what the form field states if characters in acceptable range, but no other prompt answered with y.
   } else if (alertNoty == -4) {
     return text = `Please give me something to work with.`;
+    //final catch all
   } else {
     return text = `Please answer with the text "y" for yes and "n" for no.`;
   }
 };
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
